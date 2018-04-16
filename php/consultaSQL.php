daec1528dd7c099170b7f757fb7a0412e62d73ff
Israel <isan.09725@gmail.com>
2018-04-16 16:20:29 -0500

    Parte del Login

--- php/consultaSQL.php
+++ php/consultaSQL.php
@@ -11,24 +11,58 @@
 
 <body>
     <?php
-        $usuario = htmlentities(addslashes($_POST["usuario"]));
-        $password = htmlentities(addslashes($_POST["password"]));
-        $mysqli = new mysqli('localhost', 'root', '', 'Proyecto_Final');
-        if(!$mysqli){
-            echo "No se pudo realizar la conexión PHP - MySQL"; 
+        /*$mysqli = new mysqli('localhost', 'root', '', 'Proyecto_Final');
+        if($mysqli->connect_errno){
+            echo "Fallo al conectar a MySQL: " . $mysqli->connect_error;
         }
         else{
-            $sql = "SELECT USUARIO,PASSWORD  from usuarios_pass where USUARIO = '"
-                    .$usuario."' AND PASSWORD = '". $password."'";                   
+            $usuario = htmlentities(addslashes($_POST["usuario"]));
+            $password = htmlentities(addslashes($_POST["password"]));
+            $sql = "SELECT USUARIO,PASSWORD from usuarios_pass where USUARIO = $usuario AND PASSWORD = $password ";                   
+            try{
+                $resultado = $mysqli_query($sql);
+            }catch(Exception $e){
+                echo "Error: " . $e;
+            }
             
-            $resultado = $mysqli->query($sql);
+
+            $fila = $resultado->fetch_assoc();
+            echo $fila['_msg'];
 
             if($resultado->num_rows>0){
-                echo "<h2>Adelante!!!!</h2>"
+                echo "<h2>Adelante!!!!</h2>";
             }else{
                 header("location:../modulos/Login.html");
             }
+        }*/
+
+        try{
+            $base = new PDO("mysql:host=localhost; dbname=proyecto_final", "root", "");
+            $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql = "SELECT * FROM usuarios_pass WHERE USUARIO=:usuario AND PASSWORD= :password";
+
+            $resultado = $base->prepare($sql);
+            $usuario = htmlentities(addslashes($_POST["usuario"]));
+            $password = htmlentities(addslashes($_POST["password"]));
+
+            $resultado->bindValue(":usuario",$usuario);
+            $resultado->bindValue(":password",$password);
+
+            $resultado->execute();
+            $numero_registro = $resultado->rowCount();
+
+            if($numero_registro != 0){
+                echo "<h2>Adelante!!!!</h2>";
+
+            }else{
+                header("location:../modulos/Login.html");
+            }
+
+        }catch(Exception $e){
+            die("Error: " . $e->getMessage());
         }
+
        
 
     ?>
