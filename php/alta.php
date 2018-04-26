6818e869c8000391f86a99969e7a052bd3bf19c7
Israel <isan.09725@gmail.com>
2018-04-26 17:51:53 -0500

    Actualización adelanto

---
+++ php/alta.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Crear Cuenta</h2>
+  <form>
+  <div class="form-group">
+      <label for="nombre">Nombre</label>
+      <input type="text" class="form-control" id="nombre" placeholder="Ingresa Tu Nombre">
+    </div>
+  <div class="form-group">
+      <label for="apellido">Apellido</label>
+      <input type="text" class="form-control" id="apellido" placeholder="Ingresa Tu Apellido">
+    </div>
+  <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Ingresa Tu correo">
+    </div>
+    <div class="form-group">
+      <label for="usuario">Usuario</label>
+      <input type="text" class="form-control" id="usuario" placeholder="Ingresa Tu usuario">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Ingresa Tu password">
+    </div>
+    <button type="submit" class="btn btn-default">Crear Cuenta</button>
+  </form>
+</div>
+
+</body> 
+</html>
